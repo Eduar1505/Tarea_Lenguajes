cf3111687922165124f4fed22e90c35f7c51a4b4
Eduar1505 <eduar.ventura@unah.hn>
2024-06-12 21:14:23 -0600

    ...

--- Ejercicio1.dart
+++ Ejercicio1.dart
@@ -7,8 +7,8 @@
 import 'Circulo.dart';
 
 void main() {
-  final Circulo circulorojo = Circulo(radio: 15, color: 'Rojo');
-  final Circulo circulosincolor = Circulo(radio: 10);
+  final Circulo circulorojo = Circulo(radio: 5, color: 'Rojo');
+  final Circulo circulosincolor = Circulo(radio: 2);
 
   print('Radio:  ${circulorojo.radio}');
   print('Color del circulo: ${circulorojo.color}');
